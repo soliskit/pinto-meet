cfafdb0280d48b309963245b1cee5122e1a95ef5
David Solis <github@soliskit.com>
2020-11-05 13:58:06 -0800

    add answer button and connected users list
    Only add connected users besides yourself when adding a video stream

--- public/script.js
+++ public/script.js
@@ -1,54 +1,80 @@
 const socket = io("/")
 const videoGrid = document.getElementById("video-grid")
+const connectedUsersList = document.getElementById("connected-users")
+const callControls = document.getElementById("call-controls")
 const myPeer = new Peer(undefined, { host: "/", port: "4001"})
 const myVideo = document.createElement("video")
 myVideo.muted = true
 const peers = []
+const connectedUsers = []
 
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
-    addVideoStream(myVideo, stream)
+    addVideoStream(myVideo, stream, myPeer.id)
 
     myPeer.on("call", call => {
         call.answer(stream)
         const video = document.createElement("video")
         // other users video stream
         call.on("stream", userVideoStream => {
-            addVideoStream(video, userVideoStream)
+            addVideoStream(video, userVideoStream, call.peer)
         })
     })
 
     socket.on("user-connected", userId => {
-        connectToNewUser(userId, stream)
+        const answerButton = document.createElement("button")
+        answerButton.innerText = "Answer"
+        answerButton.id = "answer" + userId
+        callControls.append(answerButton)
+        answerButton.addEventListener("click", () => {
+            connectToNewUser(userId, stream)
+            answerButton.remove()
+        })
     })
 })
 
 socket.on("user-disconnected", userId => {
     if (peers[userId]) peers[userId].close()
+    document.getElementById("answer" + userId).remove()
 })
 
 myPeer.on("open", id => {
     socket.emit("join-room", ROOM_ID, id)
 })
 
-function addVideoStream(video, stream) {
+function addVideoStream(video, stream, userId) {
+    if (connectedUsers.indexOf(userId) != -1) {
+        return
+    }
+    connectedUsers.push(userId)
     video.srcObject = stream
     video.addEventListener("loadedmetadata", () => {
         video.play()
     })
     videoGrid.append(video)
+    const userName = document.createElement("li")
+    userName.innerText = userId
+    userName.id = userId
+    connectedUsersList.append(userName)
 }
 
 function connectToNewUser(userId, stream) {
     const call = myPeer.call(userId, stream)
     const video = document.createElement("video")
+    console.log("connectToNewUser: " + userId)
     call.on("stream", userVideoStream => {
-        addVideoStream(video, userVideoStream)
+        addVideoStream(video, userVideoStream, userId)
     })
     call.on("close", () => {
-        video.remove()
+        removeVideoStream(video, userId)
     })
     peers[userId] = call
+}
+
+function removeVideoStream(video, userId) {
+    document.getElementById(userId).remove()
+    video.remove()
+    connectedUsers.remove(userId)
 }
