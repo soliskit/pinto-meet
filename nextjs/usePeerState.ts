557565c9f0d5044bf22b72314b4e1b3adc6c6b14
David Solis <github@soliskit.com>
2020-11-11 13:20:32 -0800

    fix semicolon lint error

--- nextjs/usePeerState.ts
+++ nextjs/usePeerState.ts
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import Peer, { MediaConnection } from 'peerjs';
-import { Socket } from 'socket.io-client';
+import { useEffect, useState } from 'react'
+import Peer, { MediaConnection } from 'peerjs'
+import { Socket } from 'socket.io-client'
 
 // copied partially from https://github.com/madou/react-peer/blob/master/src/use-peer-state.tsx
 const usePeerState = (
@@ -8,8 +8,8 @@ const usePeerState = (
   opts: { userId: string | undefined, roomId: string, socket: Socket } = { userId: undefined, roomId: undefined, socket: undefined }
 ): [string | undefined, Peer, PeerError | undefined] => {
   const [error, setError] = useState<PeerError | undefined>(undefined)
-  const [peer, setPeer] = useState<Peer | undefined>(undefined);
-  const [userId, setUserId] = useState(opts.userId);
+  const [peer, setPeer] = useState<Peer | undefined>(undefined)
+  const [userId, setUserId] = useState(opts.userId)
   const socket = opts.socket
   const roomId = opts.roomId
 
@@ -21,28 +21,28 @@ const usePeerState = (
           port: Number(process.env.NEXT_PUBLIC_PEER_PORT),
           secure: Boolean(process.env.NEXT_PUBLIC_PEER_SECURE)
         })
-        setPeer(localPeer);
+        setPeer(localPeer)
 
         localPeer.on('open', () => {
           if (userId !== localPeer.id) {
-            setUserId(localPeer.id);
+            setUserId(localPeer.id)
           }
           if (localPeer.id) {
             socket.emit('join-room', roomId, localPeer.id)
           }
-        });
+        })
 
         localPeer.on('error', err => setError(err))
-      });
+      })
 
       return function cleanup() {
         if (peer) {
-          peer.destroy();
+          peer.destroy()
         }
-      };
+      }
     },
     [opts.userId, stream]
-  );
+  )
 
   return [
     userId,
@@ -62,4 +62,4 @@ export interface PeerCall {
   connection: MediaConnection
 }
 
-export default usePeerState;
+export default usePeerState
