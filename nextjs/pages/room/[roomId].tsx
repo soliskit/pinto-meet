026b1cd96b748269dec6a9d362b9e65034d3d547
David Solis <github@soliskit.com>
2020-11-13 19:19:56 -0800

    remove redundant env variable

--- nextjs/pages/room/[roomId].tsx
+++ nextjs/pages/room/[roomId].tsx
@@ -13,11 +13,9 @@ const Room = () => {
   if (roomId instanceof Array) {
     throw Error('Array passed into room parameter')
   }
-  if (Boolean(process.env.NEXT_PUBLIC_PEER_SECURE) === true) {
-    socket = io(`https://${process.env.NEXT_PUBLIC_PEER_HOST}`)
-  } else {
-    socket = io(`http://${process.env.NEXT_PUBLIC_PEER_HOST}`)
-  }
+  
+  socket = io(`https://${process.env.NEXT_PUBLIC_PEER_HOST}`)
+  
   const [userid, peer, peerError] = usePeerState(stream, { userId: undefined, roomId, socket })
   const [calls] = useConnectionState(peer, socket, stream)
   let errorMessage = <p></p>
