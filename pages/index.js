a9bef49237a7a02f97f973d8718797362a636852
David Solis <github@soliskit.com>
2020-11-23 07:45:51 -0800

    set viewport

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home () {
     <div>
       <Head>
         <title>Pinto Pinto</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
