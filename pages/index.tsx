d1396fdc521db7e5c84e16b90efa721efffbf0ae
David Solis <github@soliskit.com>
2020-11-23 15:08:04 -0800

    rename home class to background

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,11 @@ import JoinForm from '../types/join-form'
 
 function Home () {
   return (
-    <div className={styles.home}>
+    <div className={styles.background}>
       <Head>
         <title>Pinto Pinto</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <div className={styles.container} >
