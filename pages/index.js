85e16b63e4b90b25fb342ed5cb75c0fbe2ecfd8c
David Solis <github@soliskit.com>
2020-11-23 04:40:45 -0800

    remove redundant closing tag

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home () {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        <Time></Time>
+        <Time/>
         </h1>
 
         <p className={styles.description}>
