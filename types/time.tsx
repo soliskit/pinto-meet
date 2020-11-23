26b6f415cfe76d0bfd4db70d3ca6024bd8ccd8d7
David Solis <github@soliskit.com>
2020-11-23 03:33:30 -0800

    create time component

---
+++ types/time.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+const Time = (): JSX.IntrinsicElements['time'] => {
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+  const weekdays = ['Sun', 'Mon', 'Tues', 'Weds', 'Thur', 'Fri', 'Sat']
+  const [date, setDate] = useState(new Date())
+  const hour = date.getHours()
+  const minute = checkMinutes(date.getMinutes())
+  const ampm = hour >= 12 ? 'PM' : 'AM';
+  const weekday = weekdays[date.getDay()]
+  const month = months[date.getMonth()]
+  const day = date.getDate()
+
+  useEffect(() => {
+    const timerID = setInterval( () => tick(), 1000 )
+    return function cleanup () {
+      clearInterval(timerID)
+    }
+  })
+  
+  function checkMinutes(m) {
+    if (m < 10) {
+      m = '0' + m
+    }
+    return m
+  }
+  function tick() {
+    setDate(new Date())
+  }
+
+  return <time>{hour}:{minute} {ampm}&nbsp; • &nbsp;{weekday}, &nbsp;{month} {day}</time>
+}
+
+export default Time
