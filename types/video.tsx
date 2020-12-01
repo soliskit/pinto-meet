ba453c4aac71976bbc1054a89cb263cd4f6853b6
David Solis <github@soliskit.com>
2020-11-30 20:40:00 -0800

    provide initialize value for videoRef
    check video for initial null value before playing stream

--- types/video.tsx
+++ types/video.tsx
@@ -2,9 +2,12 @@ import { useRef } from 'react'
 import styles from '../styles/Video.module.css'
 
 const Video = (props: { stream: MediaStream, muted: boolean }) => {
-  const videoRef = useRef<HTMLVideoElement>()
+  const videoRef = useRef<HTMLVideoElement>(null)
 
   function handleCanPlay () {
+    if (!videoRef.current) {
+      throw Error('Video Element missing source')
+    }
     return videoRef.current.play()
   }
 
