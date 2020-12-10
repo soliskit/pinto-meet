953fe98736da9296b2f3af515c30f2210e833d0d
David Solis <github@soliskit.com>
2020-12-10 01:28:28 -0800

    add unshare svg
    https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture

--- types/presenter.tsx
+++ types/presenter.tsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import Disconnect from '../public/disconnect'
 import Mute from '../public/mute'
 import Share from '../public/share'
+import UnShare from '../public/un-share'
 import UnMute from '../public/unmute'
 import styles from '../styles/Presenter.module.css'
 import Video from '../types/video'
 
 const Presenter = (props: { stream: MediaStream, disconnect: () => void }) => {
   const [micActivated, setMicActivated] = useState<boolean>(false)
+  const [screenCaptureActivated, setScreenCapture] = useState<boolean>(false)
 
   let muteButton: JSX.IntrinsicElements['button'] // eslint-disable-line no-undef
+  let shareButton: JSX.IntrinsicElements['button'] // eslint-disable-line no-undef
 
   const activateMicrophone = () => {
     setMicActivated(true)
@@ -27,25 +30,35 @@ const Presenter = (props: { stream: MediaStream, disconnect: () => void }) => {
     })
   }
 
+  const startScreenCapture = () => { setScreenCapture(true) }
+
+  const endScreenCapture = () => { setScreenCapture(false) }
+
   if (micActivated) {
     muteButton = <button onClick={muteMicrophone}><Mute/></button>
   } else {
     muteButton = <button onClick={activateMicrophone}><UnMute/></button>
   }
 
+  if (screenCaptureActivated) {
+    shareButton = <button onClick={endScreenCapture}><UnShare/></button>
+  } else {
+    shareButton = <button onClick={startScreenCapture}><Share/></button>
+  }
+
   return (
-      <div className={styles.presenterContainer}>
-        <span id={styles.presenterWrapper}>
+    <div className={styles.presenterContainer}>
+      <span id={styles.presenterWrapper}>
         <Video stream={props.stream} muted={true}/>
-          <span id={styles.controlsWrapper}>
-            <div className={styles.controlsContainer}>
-              {muteButton}
-              <button><Share/></button>
-              <button onClick={props.disconnect}><Disconnect/></button>
-            </div>
-          </span>
+        <span id={styles.controlsWrapper}>
+          <div className={styles.controlsContainer}>
+            {muteButton}
+            {shareButton}
+            <button onClick={props.disconnect}><Disconnect/></button>
+          </div>
         </span>
-      </div>
+      </span>
+    </div>
   )
 }
 
