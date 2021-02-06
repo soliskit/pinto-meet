d97ac699b7e95bcd4ba08bc593cbbe7d6210b9fb
David Solis <github@soliskit.com>
2021-02-05 23:29:11 -0800

    configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: ['./pages/**/*.tsx', './types/*.{ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', '-apple-system', 'sans-serif']
+    },
+    screens: {
+      /* eslint-disable quote-props */
+      // iPhone SE portrait
+      'sm': '320px',
+      // iPhone 8+ landscape
+      'md': '736px',
+      // iPad mini landscape & iPad Pro 12.9 portait
+      'lg': '1024px',
+      // iPad Pro 12.9 landscape
+      'xl': '1366px',
+      // Desktop
+      '2xl': '1552px'
+      /* eslint-enable quote-props */
+    },
+    extend: {
+      backgroundImage: theme => ({
+        'hero-image': "url('/hero_image.jpg')"
+      })
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
