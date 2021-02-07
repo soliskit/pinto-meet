46369831b4af7a3e54595060cec24475e3b4efbc
David Solis <github@soliskit.com>
2021-02-07 12:48:01 -0800

    incorporate presenter video into flexbox grid

--- types/attendees.tsx
+++ types/attendees.tsx
@@ -1,15 +1,10 @@
-import styles from '../styles/Attendees.module.css'
 import PeerCall from './peer-call'
 import Video from './video'
 
 const Attendees = (props: { peerCalls: PeerCall[] }) => {
   // eslint-disable-next-line no-undef
-  const videos: JSX.IntrinsicElements['video'][] = props.peerCalls.map((peerCall) => <Video stream={peerCall.stream} muted={false} key={peerCall.peerId} />)
-  return (
-    <span id={styles.presenterWrapper}>
-      {videos}
-    </span>
-  )
+  const videos: JSX.IntrinsicElements['video'][] = props.peerCalls.map((peerCall) => <div className='w-1/2' key={peerCall.peerId}><Video stream={peerCall.stream} muted={false} key={peerCall.peerId} /></div>)
+  return <>{videos}</>
 }
 
 export default Attendees
