9ff6da61c4f5405c0788d8880cf92a527b5547f7
David Solis <github@soliskit.com>
2021-02-09 00:00:08 -0800

    shrink flex box items for mobile

--- types/attendees.tsx
+++ types/attendees.tsx
@@ -3,7 +3,7 @@ import Video from './video'
 
 const Attendees = (props: { peerCalls: PeerCall[] }) => {
   // eslint-disable-next-line no-undef
-  const videos: JSX.IntrinsicElements['video'][] = props.peerCalls.map((peerCall) => <div className='' key={peerCall.peerId}><Video stream={peerCall.stream} muted={false} key={peerCall.peerId} /></div>)
+  const videos: JSX.IntrinsicElements['video'][] = props.peerCalls.map((peerCall) => <div className='w-1/3 lg:max-w-max' key={peerCall.peerId}><Video stream={peerCall.stream} muted={false} key={peerCall.peerId} /></div>)
   return <>{videos}</>
 }
 
