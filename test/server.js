59d493b46e9ed171ae55c90ce0fe096a77c95e52
David Solis <github@soliskit.com>
2021-02-19 09:52:15 -0800

    QA client server

---
+++ test/server.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+const express = require('express')
+
+const app = express()
+// TODO: - Verify test folder is serving files from absolute path of directory
+app.use(express.static(path.join(__dirname, 'test')))
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname })
+})
+
+app.get('/test/script.js', (req, res) => {
+  // sendFile() sets Content-Type response HTTP header field based on filename extension
+  res.sendFile('script.js', { root: __dirname })
+})
+
+app.listen(5000, () => {
+  console.dir('Test node client server listening on http://localhost:5000')
+})
