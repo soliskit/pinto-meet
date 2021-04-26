48e6357e3e85c8e8b36a6eb1ab8fe2dd400aa5dd
David Solis <github@soliskit.com>
2021-04-25 18:40:26 -0700

    fix roomHeader
    increment one to account for user in call

--- pages/room/[roomId].tsx
+++ pages/room/[roomId].tsx
@@ -100,24 +100,14 @@ const Room = ({
     )
   }
 
-  if (callStatus && calls.length === 0) {
-    roomHeader = (
-      <>
-        <h1>Joined | {roomName}</h1>
-        <h2>You are the 1st in the room</h2>
-      </>
-    )
-  } else if (callStatus) {
-    roomHeader = (
-      <>
-        <h1>Joined | {roomName}</h1>
-        <h2>{toCardinal(calls.length)} in the room</h2>
-      </>
-    )
-  }
-
   if (callStatus) {
     joinButton = <></>
+    roomHeader = (
+      <>
+        <h1>Joined | {roomName}</h1>
+        <h2>{toCardinal(calls.length + 1)} in the room</h2>
+      </>
+    )
   }
 
   return (
