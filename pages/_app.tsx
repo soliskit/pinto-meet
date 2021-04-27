d944a00910c5777b027d928a06bdf9e7aa3456ca
David Solis <github@soliskit.com>
2021-04-27 16:30:39 -0700

    move export of app component to last line
    relocate export statement outside of component definition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,10 @@ import { AppProps } from 'next/app'
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
 
-export default App
-
 declare global {
   interface HTMLCanvasElement {
     captureStream(frameRate?: number): MediaStream;
  }
 }
+
+export default App
