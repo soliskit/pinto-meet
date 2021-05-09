f538a76ac82c6aa814a52f320a1d6767f1284c55
David Solis <github@soliskit.com>
2021-05-09 13:58:48 -0700

    encapsulate image object into draw() function
    Legend:
    (1 Action) == An action taken by the first person who joins the room
    (2 Action) == An action taken by the 2nd person who joins the room
    
    Tested paths that work:
    (½  Open) => (1 Photo) => (1 Join) => (2 Join) => (1 Redraw x2)
    (½  Open) => (1 Join) => (1 Photo) => (2 Join) => (1 Redraw x2)
    (½  Open) => (1 Join) => (2 Join) => (2 Photo) => (1 Redraw x2)
    (½  Open) => (1 Join) => (2 Join) => (1 Photo) => (2 Redraw x2)

--- types/photo-uploader.tsx
+++ types/photo-uploader.tsx
@@ -1,4 +1,4 @@
-import { RefObject, useEffect, useRef, useState } from 'react'
+import { RefObject, useEffect, useState } from 'react'
 import Peer from 'peerjs'
 
 const PhotoUploader = (
@@ -9,44 +9,41 @@ const PhotoUploader = (
     peer: Peer | null
   }) => {
   const defaultPhoto = '/no-video.svg'
-  const [photo, setPhoto] = useState(defaultPhoto)
-  const imageRef = useRef<HTMLImageElement>(new Image())
+  const [photo, setPhoto] = useState<string>(defaultPhoto)
 
-  const draw = (context: CanvasRenderingContext2D) => {
+  const draw = () => {
+    const canvas = props.canvasRef.current
+    const context = canvas?.getContext('2d')
+    if (!canvas || !context) {
+      console.dir('Canvas or Contex NULL')
+      return
+    }
     let imageWidth: number
     let imageHeight: number
-    const image = imageRef.current
+    const image = new Image()
     image.src = photo
     image.decode().then(() => {
       console.dir('IMAGE DECODED')
       console.dir(`PEER: ${props.peer}`)
       console.dir(`STREAM: ${props.stream}`)
-    //   const ratio = Math.min(288 / image.width, 288 / image.height)
-    //   if (image.width > 288 || image.height > 288) {
-    //     imageWidth = image.width * ratio
-    //     imageHeight = image.height * ratio
-    //   } else {
-    //     imageWidth = image.width
-    //     imageHeight = image.height
-    //   }
-    //   const x = context.canvas.width / 2 - imageWidth / 2
-    //   const y = context.canvas.height / 2 - imageHeight / 2
-    //   context.clearRect(0, 0, context.canvas.width, context.canvas.height)
-    //   context.drawImage(image, x, y, imageWidth, imageHeight)
-    // })
-    // console.dir('decode')
-    // const ratio = Math.min(288 / image.width, 288 / image.height)
-    // if (image.width > 288 || image.height > 288) {
-    //   imageWidth = image.width * ratio
-    //   imageHeight = image.height * ratio
-    // } else {
-    //   imageWidth = image.width
-    //   imageHeight = image.height
-    // }
-    // const x = context.canvas.width / 2 - imageWidth / 2
-    // const y = context.canvas.height / 2 - imageHeight / 2
-    // context.clearRect(0, 0, context.canvas.width, context.canvas.height)
-    // context.drawImage(image, x, y, imageWidth, imageHeight)
+      const ratio = Math.min(288 / image.width, 288 / image.height)
+      if (image.width > 288 || image.height > 288) {
+        imageWidth = image.width * ratio
+        imageHeight = image.height * ratio
+      } else {
+        imageWidth = image.width
+        imageHeight = image.height
+      }
+      const x = context.canvas.width / 2 - imageWidth / 2
+      const y = context.canvas.height / 2 - imageHeight / 2
+      if (props.peer) {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+        context.drawImage(image, x, y, imageWidth, imageHeight)
+        console.dir('CAPTURING STREAM')
+        props.streamDidChange(canvas.captureStream())
+      } else {
+        console.dir('SKIP CAPTURING STREAM')
+      }
     })
   }
 
@@ -60,46 +57,8 @@ const PhotoUploader = (
     setPhoto(defaultPhoto)
   }
 
-  const redraw = () => {
-    if (!props.canvasRef.current) {
-      throw Error('Missing canvas element reference')
-    }
-    const canvas = props.canvasRef.current
-    const context = canvas.getContext('2d')
-    let imageWidth: number
-    let imageHeight: number
-    const image = imageRef.current
-    if (context) {
-      console.dir('redrawing')
-      console.dir(props.peer)
-      console.dir(props.stream)
-      const ratio = Math.min(288 / image.width, 288 / image.height)
-      if (image.width > 288 || image.height > 288) {
-        imageWidth = image.width * ratio
-        imageHeight = image.height * ratio
-      } else {
-        imageWidth = image.width
-        imageHeight = image.height
-      }
-      const x = context.canvas.width / 2 - imageWidth / 2
-      const y = context.canvas.height / 2 - imageHeight / 2
-      context.clearRect(0, 0, context.canvas.width, context.canvas.height)
-      context.drawImage(image, x, y, imageWidth, imageHeight)
-    }
-  }
-
   useEffect(() => {
-    if (!props.canvasRef.current) {
-      throw Error('Missing canvas element reference')
-    }
-    const canvas = props.canvasRef.current
-    const context = canvas.getContext('2d')
-    if (props.peer) {
-      props.streamDidChange(canvas.captureStream())
-    }
-    if (context) {
-      draw(context)
-    }
+    draw()
     return function cleanup() {
       console.dir("cleanup <PhotoUploader>'s useEffect")
     }
@@ -116,7 +75,7 @@ const PhotoUploader = (
         />
       </form>
       <button onClick={removePhoto}>Remove photo</button>
-      <button onClick={redraw}>ReDraw</button>
+      <button onClick={draw}>ReDraw</button>
     </div>
   )
 }
